c39ed551cc17deda1b82a0e4a3db69d51a264ef4
Nabin Islam <sinabin62246@gmail.com>
2024-05-20 23:31:51 +0600

    initial project setup done

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+//routes
+
+
+app.get('/', (req: Request, res: Response) =>
+  res.send(`Server is running fine!`),
+);
+
+export default app;
