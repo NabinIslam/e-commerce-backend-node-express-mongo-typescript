803761eacaa7e438db3d4f749cb3edb4b4221162
Nabin Islam <sinabin62246@gmail.com>
2024-05-21 01:36:31 +0600

    product interface and model done

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,68 @@
+import { Schema, model } from 'mongoose';
+import { TProduct } from './product.interface';
+
+const variantSchema = new Schema({
+  type: { type: String, required: [true, 'Variant type is required'] },
+  value: { type: String, required: [true, 'Variant value is required'] },
+});
+
+const inventorySchema = new Schema({
+  quantity: {
+    type: Number,
+    required: [true, 'Inventory quantity is required'],
+    min: [0, 'Inventory quantity cannot be less than 0'],
+  },
+  inStock: {
+    type: Boolean,
+    required: [true, 'Inventory inStock status is required'],
+  },
+});
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name is required'],
+    minlength: [3, 'Product name must be at least 3 characters long'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Product description is required'],
+    minlength: [10, 'Product description must be at least 10 characters long'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required'],
+    min: [0, 'Product price must be a positive number'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Product category is required'],
+    minlength: [3, 'Product category must be at least 3 characters long'],
+  },
+  tags: {
+    type: [String],
+    required: [true, 'Product tags are required'],
+    validate: {
+      validator: function (v: string[]) {
+        return v.length > 0;
+      },
+      message: 'There must be at least one tag',
+    },
+  },
+  variants: {
+    type: [variantSchema],
+    required: [true, 'Product variants are required'],
+    validate: {
+      validator: function (v: string[]) {
+        return v.length > 0;
+      },
+      message: 'There must be at least one variant',
+    },
+  },
+  inventory: {
+    type: inventorySchema,
+    required: [true, 'Product inventory is required'],
+  },
+});
+
+export const Product = model<TProduct>('Product', productSchema);
